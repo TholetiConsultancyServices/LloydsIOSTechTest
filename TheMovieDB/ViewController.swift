8f7f5996a6d1312155e45f4474c9d1727a765d53
Appaji Tholeti <appajit@gmail.com>
2022-03-06 14:14:06 +0000

    Initial Commit

---
+++ TheMovieDB/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TheMovieDB
+//
+//  Created by Appaji Tholeti on 06/03/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
